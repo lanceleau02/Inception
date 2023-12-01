2857aa04174aa36a3baf87446aa413762158bb35
lanceleau02 <lancelot.prieure@gmail.com>
2023-12-01 13:05:07 +0100

    need to finish some config

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
