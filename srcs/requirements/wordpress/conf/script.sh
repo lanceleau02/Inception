b849bbad12540da21a62e1ec86025fe0fa46b0f2
lanceleau02 <lancelot.prieure@gmail.com>
2023-12-06 17:39:01 +0100

    Thx stales for the help

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-sleep 10
+echo $MYSQL_USER
+
+cd /var/www/html/
 
 if [ -f "/var/www/html/wordpress/wp-config.php" ]
 then
 	echo "Wordpress already installed and setup"
 else
-	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
 	wp core download --allow-root
-	sleep 5
-	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --skip-check
-	wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --extra-php
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 	wp user create --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role='editor'
 fi
 
