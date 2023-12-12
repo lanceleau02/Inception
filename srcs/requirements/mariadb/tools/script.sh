2cf10861bf090270c0bde86f7cd47991023810aa
lanceleau02 <lancelot.prieure@gmail.com>
2023-12-12 15:58:34 +0100

    All is fine, I think

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,12 +2,9 @@
 service mariadb start
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e -p$MYSQL_ROOT_PASSWORD "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e -p$MYSQL_ROOT_PASSWORD "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO 'root'@'localhost' IDENTIFIED BY \`${MYSQL_ROOT_PASSWORD}\` WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD(\`$MYSQL_ROOT_PASSWORD\`);"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e -u root "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 sleep 1
-mysqladmin -u root shutdown
-#mysqld --bind-address=0.0.0.0
+service mariadb stop
 exec mysqld_safe
